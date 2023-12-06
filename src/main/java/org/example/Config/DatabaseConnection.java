58ceab021875ed5901bf653d4b41a3be94dccfe8
Ngitangita <hei.elise.3@gmail.com>
2023-12-06 15:26:28 +0200

    added packeg configuration

---
+++ src/main/java/org/example/Config/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package org.example.Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static Connection getConnection(){
+        try {
+            return DriverManager.getConnection (
+                    System.getenv ( "PG_URL" ),
+                    System.getenv ( "PG_USER" ),
+                    System.getenv ( "PG_PASSWORD" )
+            );
+        } catch ( SQLException e ) {
+            throw new RuntimeException ( e );
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,10 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args){
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print ( "Hello and welcome!" );
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println ( "i = " + i );
-        }
+        System.out.println (  System.getenv ( "PG_URL" ) );
+        System.out.println (  System.getenv ( "PG_USER" ) );
+        System.out.println (  System.getenv ( "PG_PASSWORD" ) );
     }
 }
