74751c84a1a24c0dcb8b19b10b28680d0d0a6f60
Hevitriniavo <hei.tantely@gmail.com>
2023-12-14 06:33:56 +0300

    use lombok for easy the code

--- src/main/java/com/dev/sql/Table.sql
+++ src/main/java/com/dev/sql/Table.sql
@@ -1,43 +1,43 @@
 
 -- Table for currencies
         CREATE TABLE IF NOT EXISTS "currency" (
-        id serial PRIMARY KEY,
-        name VARCHAR(255),
-        code VARCHAR(4)
+            id serial PRIMARY KEY,
+            name VARCHAR(255),
+            code VARCHAR(4)
         );
 
         -- Inserting currencies
         INSERT INTO "currency" ( name, code) VALUES
-        ('Euro', 'EUR'),
-        ( 'Ariary', 'AR');
+        ('EURO', 'EUR'),
+        ( 'ARIARY', 'AR');
 
         -- Table for accounts
         CREATE TABLE IF NOT EXISTS "account" (
-        id serial PRIMARY KEY,
-        name VARCHAR(255),
-        balance float,
-        last_update_date_time timestamp,
-        currency_id INT,
-        account_type VARCHAR(20),
-        FOREIGN KEY ( currency_id ) REFERENCES "currency"(id)
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255),
+            balance float,
+            last_update_date_time timestamp,
+            currency_id INT,
+            account_type VARCHAR(20),
+            FOREIGN KEY ( currency_id ) REFERENCES "currency"(id)
         );
 
         -- Inserting account types
         INSERT INTO "account" (name, balance, last_update_date_time, currency_id, account_type) VALUES
-        ( 'Current Account', 0.00, NOW(), 2, 'Bank'),
-        ('Savings Account', 0.00, NOW(), 2, 'Bank'),
-        ('Cash Wallet', 0.00, NOW(), 2, 'Cash'),
-        ('Mobile Money', 0.00, NOW(), 2, 'Mobile Money');
+        ( 'Current Account', 19.00, NOW(), 2, 'Bank'),
+        ('Savings Account', 12.00, NOW(), 2, 'Bank'),
+        ('Cash Wallet', 15.00, NOW(), 2, 'Cash'),
+        ('Mobile Money', 188.00, NOW(), 2, 'Mobile Money');
 
         -- Table for transactions
         CREATE TABLE IF NOT EXISTS "transaction" (
-        id serial PRIMARY KEY,
-        label VARCHAR(255),
-        amount float,
-        date_time timestamp,
-        transaction_type VARCHAR(10),
-        account_id INT,
-        FOREIGN KEY (account_id) REFERENCES "account"(id)
+            id serial PRIMARY KEY,
+            label VARCHAR(255),
+            amount float,
+            date_time timestamp,
+            transaction_type VARCHAR(10),
+            account_id INT,
+            FOREIGN KEY (account_id) REFERENCES "account"(id)
         );
 
         -- Inserting a transaction
