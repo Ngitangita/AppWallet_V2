64be4c8fd74a6d9c43535770a507c8e55c266ea8
Hevitriniavo <hei.tantely@gmail.com>
2023-12-15 13:27:44 +0300

    add test account crud test

--- src/main/java/com/fonctionality/Main.java
+++ src/main/java/com/fonctionality/Main.java
@@ -1,21 +1,29 @@
 package com.fonctionality;
 
-import com.fonctionality.entity.Account;
+import com.fonctionality.entity.*;
 import com.fonctionality.repository.AccountRepository;
 import com.fonctionality.repository.CurrencyRepository;
 import com.fonctionality.repository.TransactionRepository;
 import com.fonctionality.service.AccountService;
 
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args){
+
+        Currency currency = Currency.builder()
+                .id(3L)
+                .code(CodeCurrency.AR)
+                .name(NameCurrency.ARIARY)
+                .build();
         CurrencyRepository currencyRepository = new CurrencyRepository();
         AccountRepository accountRepository = new AccountRepository(currencyRepository);
         TransactionRepository transactionRTransaction = new TransactionRepository(accountRepository);
         AccountService accountService = new AccountService ( accountRepository );
-        Account debit = accountRepository.findById ( 3L );
-        Account credit = accountRepository.findById ( 4L );
-        System.out.println(accountService.carryOfTransfer(credit, debit, 203));
+        System.out.println(accountRepository.deleteById(11L));
 
     }
 
