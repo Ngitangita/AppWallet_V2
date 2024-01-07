5cd01d66536e6afea7701cf9b69c43391ff8bf9c
Ngitangita <hei.elise.3@gmail.com>
2024-01-07 10:52:05 +0300

    feat: added test of method save of transfer history

--- src/main/java/repositories/TransferHistoryRepository.java
+++ src/main/java/repositories/TransferHistoryRepository.java
@@ -1,18 +1,52 @@
 package repositories;
 
+import config.DatabaseConnection;
+import entitries.Account;
 import entitries.TransferHistory;
-import lombok.AllArgsConstructor;
+import exceptions.TransferHistoryError;
+import lombok.RequiredArgsConstructor;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class TransferHistoryRepository implements CrudOperations<TransferHistory, Long>{
 
-    private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
 
     @Override
     public List<TransferHistory> findAll(){
-        return null;
+        Connection connection = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<TransferHistory> transferHistories = new ArrayList<> ();
+        final String query = "SELECT * FROM \"transfer_history\"";
+        try  {
+            connection = DatabaseConnection.getConnection();
+            stmt = connection.prepareStatement(query);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                long debitTransactionId = rs.getLong("debit_transaction_id");
+                long creditTransactionId = rs.getLong("credit_transaction_id");
+                Account debitTransaction = (debitTransactionId != 0L) ? this.accountRepository.findById(debitTransactionId) : null;
+                Account creditTransaction = (creditTransactionId != 0L) ? this.accountRepository.findById(creditTransactionId) : null;
+                TransferHistory transferHistory = TransferHistory.builder()
+                        .id ( rs.getLong ( "id" ) )
+                        .transferDate ( rs.getTimestamp ( "transfer_date" ).toLocalDateTime ( ) )
+                        .debitTransaction ( debitTransaction )
+                        .creditTransaction ( creditTransaction )
+                        .build();
+                transferHistories.add(transferHistory);
+            }
+
+            return transferHistories;
+        } catch ( SQLException e) {
+            e.printStackTrace ();
+            throw new TransferHistoryError ("Error retrieving accounts from database");
+        } finally {
+            blockFinnally ( connection, stmt, rs );
+        }
     }
 
     @Override
@@ -27,7 +61,43 @@ public class TransferHistoryRepository implements CrudOperations<TransferHistory
 
     @Override
     public TransferHistory save(TransferHistory toSave){
-        return null;
+        final String query = "INSERT INTO \"transfer_history\" ( debit_transaction_id , credit_transaction_id , transfer_date) VALUES ( ?, ?, ?)";
+        Connection connection = null;
+        PreparedStatement stmt = null;
+
+        try {
+            connection = DatabaseConnection.getConnection();
+            stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            this.setTransferHistory( toSave, stmt );
+            int rows = stmt.executeUpdate();
+            if (rows > 0) {
+                try  {
+                    ResultSet keys = stmt.getGeneratedKeys();
+                    if (keys.next()) {
+                        Long generatedId = keys.getLong(1);
+                        toSave.setId(generatedId);
+                        return toSave;
+                    }
+                    throw new TransferHistoryError ("Error saving transfer history");
+                } catch ( SQLException e ){
+                    e.printStackTrace ();
+                    throw new TransferHistoryError ("Error saving transaction");
+                }
+            }
+            throw new TransferHistoryError ("Error saving transfer history");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new TransferHistoryError ("Error saving transfer history");
+        }finally {
+            try {
+                if (stmt != null) stmt.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new TransferHistoryError ("Error closing database related resources ");
+            }
+        }
     }
 
     @Override
@@ -44,4 +114,30 @@ public class TransferHistoryRepository implements CrudOperations<TransferHistory
     public TransferHistory deleteById(Long id){
         return null;
     }
+
+    private void setTransferHistory(TransferHistory toSave, PreparedStatement stmt) throws SQLException {
+        Account creditTransaction = toSave.getCreditTransaction ();
+        Account debitTransaction = toSave.getDebitTransaction ();
+        if (debitTransaction.getId () != null && creditTransaction.getId () != null){
+            stmt.setLong ( 1,  debitTransaction.getId ());
+            stmt.setLong ( 2,  creditTransaction.getId ());
+        } else {
+            Account accountSaved = this.accountRepository.save ( debitTransaction );
+            Account accountSavedCredit = this.accountRepository.save ( creditTransaction );
+            stmt.setLong ( 1,  accountSaved.getId ());
+            stmt.setLong ( 2,  accountSavedCredit.getId ());
+        }
+        stmt.setTimestamp ( 3, Timestamp.valueOf ( toSave.getTransferDate () ) );
+    }
+
+
+    private void blockFinnally(Connection connection, PreparedStatement stmt, ResultSet rs){
+        createStmt ( connection, stmt, rs );
+    }
+
+    static void createStmt(Connection connection, PreparedStatement stmt, ResultSet rs){
+        TransactionRepository.finnally ( connection, stmt, rs );
+    }
+
+
 }
---
+++ src/test/java/repositories/TransferHistoryRepositoryTest.java
@@ -0,0 +1,76 @@
+package repositories;
+
+import entitries.Account;
+import entitries.TransferHistory;
+import exceptions.TransferHistoryError;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TransferHistoryRepositoryTest {
+    private AccountRepository accountRep = null;
+    private TransferHistoryRepository subject = null;
+
+
+    @BeforeEach
+    void setUp() {
+        CurrencyRepository currencyRep = new CurrencyRepository ();
+         accountRep = new AccountRepository ( currencyRep );
+        subject = new TransferHistoryRepository (accountRep  );
+    }
+
+
+    @Test
+    void testFindAllTransferHistorySuccess() {
+        assertDoesNotThrow ( () -> {
+            int size = 1;
+            List<TransferHistory> transferHistories = subject.findAll();
+            assertNotNull(transferHistories, "List of transferHistory should not be null");
+            assertTrue (transferHistories.isEmpty(), "List of transferHistory should not be empty");
+            assertEquals(size, transferHistories.size(),  "List should contain four transferHistory");
+        } );
+    }
+
+    @Test
+    void testSaveTransferHistorySuccess() {
+        assertDoesNotThrow ( () -> {
+            Long creditId = 12L;
+            Long debitId = 13L;
+            Account credit = accountRep.findById ( creditId );
+            Account debit = accountRep.findById ( debitId );
+            TransferHistory transferHistory = TransferHistory.builder()
+                    .transferDate ( LocalDateTime.now () )
+                    .creditTransaction ( credit )
+                    .debitTransaction ( debit )
+                    .build();
+            List<TransferHistory> beforeTransferHistories = subject.findAll();
+            TransferHistory savedTransferHistory = subject.save ( transferHistory );
+            List<TransferHistory> transferHistories = subject.findAll();
+            assertNotNull(transferHistories, "List of transferHistory should not be null");
+            assertNotNull (savedTransferHistory.getId (), "the id transfer history should  be not null");
+            assertFalse (transferHistories.isEmpty(), "List of transferHistory should not be empty");
+            assertNotEquals(beforeTransferHistories.size(), transferHistories.size(),  "List should contain four transferHistory");
+        } );
+    }
+
+
+    @Test
+    void testTransferHistorySQLException() {
+        TransferHistoryError exception = assertThrows( TransferHistoryError.class, () -> {
+            throw new TransferHistoryError ( "Error retrieving accounts from database" );
+        });
+        assertEquals("Error retrieving accounts from database", exception.getMessage(), "should be equal");
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        accountRep = null;
+        subject = null;
+    }
+}
